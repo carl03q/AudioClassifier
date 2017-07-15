98161809461770c4b0efa74654b1537c925d0d61
Carlos Quintero <carl03q@gmail.com>
2017-07-15 14:34:14 -0500

    Updated to call get_datset
    update function call with its new parameters

--- train.py
+++ train.py
@@ -3,17 +3,26 @@ from lib import datasetManager
 
 parser = argparse.ArgumentParser()
 
-#Opciones
+# Opciones
 parser.add_argument("--val-ratio", help="Validation ratio, only needed on train mode. Default at .3", default='.3')
 parser.add_argument("--test-ratio", help="Test ratio, only needed on test mode. Default at .1", default='.1')
+parser.add_argument("--dataset", help="Dataset path. Default is ./Data/Dataset", default='./Data/Dataset/')
+parser.add_argument("--nb-per-class", help="Number of images per class. Default is 200", default='200')
+parser.add_argument("--img-size", help="Images dimensions. Default 128", default='128')
 
 args = parser.parse_args()
 
-validationRatio = args.val_ratio
-testRatio = args.test_ratio
+val_ratio = args.val_ratio
+test_ratio = args.test_ratio
+datasetPath = args.dataset
+nb_per_class = args.nb_per_class
+img_size = args.img_size
 
-print("---- Configuración ----")
-print("Validation ratio: {}".format(validationRatio))
-print("Test ratio: {}".format(testRatio))
+print("---- Configuration ----")
+print("Validation ratio: {}".format(val_ratio))
+print("Test ratio: {}".format(test_ratio))
+print("Dataset path: {}".format(datasetPath))
+print("Number of images per class: {}".format(nb_per_class))
+print("Images dimensions: {}".format(nb_per_class))
 
-datasetManager.get_dataset()
+datasetManager.get_dataset(datasetPath, nb_per_class, img_size, val_ratio, test_ratio, "train")
