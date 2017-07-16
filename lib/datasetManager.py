67d92b28362b00c6c4b0ff38157a18d8adc45c8d
ChalyKP <charlykam-13@hotmail.com>
2017-07-16 17:47:34 -0500

    Update datasetManager.py

--- lib/datasetManager.py
+++ lib/datasetManager.py
@@ -32,87 +32,44 @@ def get_dataset(slices_path, dataset_path, nb_per_class, slice_size, val_ratio, 
 
 
 def create_dataset(input_path, output_path, nb_per_class, slice_size, val_ratio, test_ratio):
-    '''
-    Chaly
-    input path es para imagenes, outputpath es para donde guardar el dataset
+    x = []
+    y = []
 
-    Debes crear una funcion que utilizando path
-    luego lee las imagenes, te puede servir os.listdir
-
-    Eso te mostrará los folder ['jazz', 'dubstep']
-    luego puedes concatenar path y lo que sale en el arreglo
-    para que te quede algo como ./Data/Slices/jazz y le aplicas lisdir otra vez
-    eso te mostrara todos los archivos de imagenes
-
-    la vaina es que al final debes leer todas esas imagenes en un arreglo
-    un buen metodo es utilizar un for a ese arreglo y dentro
-
-    Image.open('direccion de la imagen') #Image es parte de libreria PIL
-
-    a Cada imagen debes hacerle un proceso
-    1. .resize(slice_size, slice_size, resample=Image.ANTIALIAS)
-    2. normalizar -> dividir entre 255 -> img/255.0
-
-    3. onehot encode (etiquetar las imagenes ->
-    label = [0 for _ in range(len(cantidad de generos)]
-    label[# de genero de la imagen] = 1
-
-    supongamos que tenemos dubstep y jazz, dubstep sería 0 y jazz 1
-
-    label = [0 for _ in range(len(cantidad de generos)]
-    esto daría [0,0] xq son 2 generos creara un arreglo de 0 para un rango de 2
-
-    si estas evaluando las imagenes de jazz
-    label [1] = 1  ....jazz es el genero segundo , tiene indice 1
-    label -> queda como [0,1]
-
-    al final tengas dos arreglos
-    uno X para las imagenes
-    y uno Y para las etiquetas
-
-    X.append(img)
-    y.append(label)
-
-    #casi terminas, ultimo paso
-    en machine learning siempre necesitas separar tus datos
-    en conjuntos, uno para entrenar, validar y testear
-
-    arriba  yo te doy un ratio  de validacion y uno de test, lo que sobra es para training
-    que debes hacer, debes repartir el arreglo X en cada set ejemplo
-
-    #por cierto, necesitas convertir el arreglo a  numpy
-
-    cantidad_validacion = int(len(X) * validationRatio)    si tengo 1000 imagenes * 0.3 = 300
-    cantidad_testing =                                     si tengo 1000 imagenes * 0.1 = 100
-
-    cantidad_entrenamiento = len(X) - cantidad_validacion - cantidad_testing      1000-300-100 + 600
-    train_X = np.array( X[:cantidad_entrenamiento] )     si tengo 1000 imagenes le digo que agarre las primeras 600 imagenes
-    train_y = np.array( y[...])
-
-    train_X.reshape([-1, sliceSize, sliceSize, 1]) //necesario para tensorflow, cambia estructura de la matriz
-
-    y haces lo mismo para validation y testing... pero alla es más complicado que las 600 primeras
-    seria algo mas como de la 600 a la 900  validacion
-
-    POR ULTIMO GRABARLOS
-    pickle.dump(train_X, open("{}train_X_{}.p".format(datasetPath, datasetName), "wb"))
-    pickle.dump(train_y, open("{}train_y_{}.p".format(datasetPath, datasetName), "wb"))
-    pickle.dump(validation_X, open("{}validation_X_{}.p".format(datasetPath, datasetName), "wb"))
-    pickle.dump(validation_y, open("{}validation_y_{}.p".format(datasetPath, datasetName), "wb"))
-    pickle.dump(test_X, open("{}test_X_{}.p".format(datasetPath, datasetName), "wb"))
-    pickle.dump(test_y, open("{}test_y_{}.p".format(datasetPath, datasetName), "wb"))
-'''
-    
-       #codigo que trabajara con los directorios
-    #path variable en donde se asignara el path de las imaegenes
     genres = os.listdir(input_path)
-    
     for genre in genres:
-        genre_dir = input_path + genre
+        genre_dir = input_path + genre +'/'
+        files = os.listdir(genre_dir)
+        print("--------------------------------------------------------------------")
+        for file in files:
+                file_dir = genre_dir + file
+                im = Image.open(file_dir)
+                print(file_dir)
+                imr = im.resize((int(slice_size), int(slice_size)), resample=Image.ANTIALIAS)
+                imgData = np.asarray(imr, dtype=np.uint8).reshape(int(slice_size), int(slice_size), 1)
+                imgData = imgData / 255.
+                x.append(imgData)
+                # Se utiliza el if para controlar los generos y asi poder crear las etiquetas para cada genero
+                if genre == 'dubstep':
+                    label = [0 for genre in range(len(genres))]
+                    label[0] = 1
+                    y.append(label)
+                elif genre == 'Jazz':
+                    label = [0 for genre in range(len(genres))]
+                    label[1] = 1
+                    y.append(label)
+    #print(y)
+    #print("--------------------------------------------------------------------")
+    # impresion de prueba del arreglo de los labels
+    #for m in x:
+       #print(m)
+    #    array_x = np.array(m)
+    #    print(array_x)
+    print("--------------------------------------------------------------------")
+    #impresion de prueba del arreglo de los labels
+    for n in y:
+       print(n)
+        #array_y = np.array(n)
     
-    print(genre_dir)     
-    return (genres)
-    
-    return path
+    #return path
 
 
