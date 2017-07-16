20993e518ca413f9d0316824bfbc98ecb9d5b25c
Carlos Quintero <carl03q@gmail.com>
2017-07-16 18:06:34 -0500

    Update train script typage, modelfit
    Improve data typage
    improve model fit parameters

--- train.py
+++ train.py
@@ -13,21 +13,21 @@ parser.add_argument("--val-ratio", help="Validation ratio, only needed on train 
 parser.add_argument("--test-ratio", help="Test ratio, only needed on test mode. Default at .1", default='.1')
 parser.add_argument("--source", help="Source path. Default is ./Data/Slices/", default='./Data/Slices/')
 parser.add_argument("--dataset", help="Dataset path. Default is ./Data/Dataset/", default='./Data/Dataset/')
-parser.add_argument("--nb-per-class", help="Number of images per class. Default is 200", default='200')
+parser.add_argument("--nb-per-class", help="Number of images per class. Default is 1000", default='1000')
 parser.add_argument("--img-size", help="Images dimensions. Default 128", default='128')
 parser.add_argument("--nb-epochs", help="Number of epochs to be executed. Default 20", default='20')
-parser.add_argument("--batch-size", help="Size of each epoch, tweak this value if you have memory issues. Default 128", default='128')
+parser.add_argument("--batch-size", help="Size of each epoch, tweak this value if you have memory issues. Default 32", default='32')
 
 args = parser.parse_args()
 
-val_ratio = args.val_ratio
-test_ratio = args.test_ratio
+val_ratio = float(args.val_ratio)
+test_ratio = float(args.test_ratio)
 source_path = args.source
 datasetPath = args.dataset
-nb_per_class = args.nb_per_class
-img_size = args.img_size
-nb_epochs = args.nb_epochs
-batch_size = args.batch_size
+nb_per_class = int(args.nb_per_class)
+img_size = int(args.img_size)
+nb_epochs = int(args.nb_epochs)
+batch_size = int(args.batch_size)
 
 print("---- Configuration ----")
 print("Validation ratio: {}".format(val_ratio))
@@ -56,12 +56,13 @@ filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
 callbacks_list = [checkpoint]
 
+
 print("Training the model...")
 model.fit(train_X, train_y,
-          batch_size=batch_size,
           epochs=nb_epochs,
+          batch_size=batch_size,
           shuffle=True,
-          validation_set=(validation_X, validation_y),
+          validation_data=(validation_X, validation_y),
           #validation_split=0.33,
           callbacks=callbacks_list)
 
