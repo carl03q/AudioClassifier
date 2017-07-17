f904802e67f081b7b46ac6eab72241ec656dc91d
Carlos Quintero <carl03q@gmail.com>
2017-07-17 10:31:46 -0500

    Include create spectrogram function

--- lib/audio2Image.py
+++ lib/audio2Image.py
@@ -1,4 +1,8 @@
 import os
+from subprocess import Popen, PIPE, STDOUT
+
+
+currentPath = os.path.dirname(os.path.realpath(__file__))[:-3]
 
 
 def convert_to_spectrogram(input_dir, output_dir):
@@ -7,9 +11,25 @@ def convert_to_spectrogram(input_dir, output_dir):
 
         print("\nCreating spectrograms ...")
 
-        '''
-        Alexi's Code
-        '''
+        genres = os.listdir(input_dir)
+        for genre in genres:
+            genre_dir = input_dir + genre + '/'
+            spec_dir = output_dir + genre + '/'
+
+            os.makedirs(os.path.dirname(spec_dir), exist_ok=True)
+            print("Converting " + genre_dir + '...')
+            pixels_per_second = 50
+            command = "a=0;" \
+                      "for file in "+genre_dir+"*.mp3; do " \
+                        "a=$((a+1));" \
+                        "out_name="+spec_dir+"$a.png;" \
+                        'sox "$file" -n spectrogram -Y 130 -X '+pixels_per_second+' -l -r -m -o $out_name;'\
+                        "echo $file; echo $out_name;"\
+                      "done"
+            p = Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True, cwd=currentPath)
+            output, errors = p.communicate()
+            if errors:
+                print(errors)
 
         print("Spectrogram successfully created ...")
     else:
