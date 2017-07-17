dddef747dbe95b2d787aa6c6e265efa2a968c16a
Carlos Quintero <carl03q@gmail.com>
2017-07-17 11:01:09 -0500

    Include slicer

--- lib/audio2Image.py
+++ lib/audio2Image.py
@@ -1,5 +1,6 @@
 import os
 from subprocess import Popen, PIPE, STDOUT
+from PIL import Image
 
 
 currentPath = os.path.dirname(os.path.realpath(__file__))[:-3]
@@ -18,7 +19,7 @@ def convert_to_spectrogram(input_dir, output_dir):
 
             os.makedirs(os.path.dirname(spec_dir), exist_ok=True)
             print("Converting " + genre_dir + '...')
-            pixels_per_second = 50
+            pixels_per_second = "50"
             command = "a=0;" \
                       "for file in "+genre_dir+"*.mp3; do " \
                         "a=$((a+1));" \
@@ -46,11 +47,33 @@ def convert_to_slices(input_dir, output_dir):
 
         print("\nChopping spectrograms into slices ...")
 
-        '''
-        Beckford's Code
-        '''
+        genres = os.listdir(input_dir)
+        for genre in genres:
+            genre_dir = input_dir + genre + '/'
+            slice_dir = output_dir + genre + '/'
+            files = os.listdir(genre_dir)
+
+            os.makedirs(os.path.dirname(slice_dir), exist_ok=True)
+
+            for idx_song, file in enumerate(files):
+                dirImage = genre_dir + file
+
+
+                with Image.open(dirImage) as img:
+                    print(dirImage)
+                    minWidth =0
+                    maxWidth =128
+                    n = int (img.size[0]/128)
+                    height = img.size[1]
+                    for idx_spec in range(n):
+                        newImg = img.crop((minWidth,0,maxWidth,height))
+                        savestring = slice_dir+"/"+str(idx_song)+"_"+str(idx_spec)+".png"
+                        newImg.save(savestring)
+                        minWidth+=128
+                        maxWidth+=128
 
         print("Slices successfully created ...")
+
     else:
         print("\n***Slices images not created***")
         print("Dir {} not found.".format(input_dir))
