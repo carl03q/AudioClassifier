68ec61f04a062937c6fb97d69b134e39fdc76f1d
Carlos Quintero <carl03q@gmail.com>
2017-07-16 20:09:11 -0500

    updated spectrogram treatment

--- classifier.py
+++ classifier.py
@@ -1,9 +1,11 @@
 import numpy as np
 import argparse
 import os
+import shutil
 
-#from keras.models import load_model
-#from PIL import Image
+# from keras.models import load_model
+# from PIL import Image
+from lib import audio2Image
 
 
 parser = argparse.ArgumentParser()
@@ -37,19 +39,34 @@ def predict_image(model, image_name, image_size):
     n = random.uniform(0, 1)
     return [n, 1-n]
 
-####################model = load_model('music.h5')
+
+# Call model
+# model = load_model('music.h5')
 input_size = 128
 
-# For loop of each image generated with de audio
-tmp_dir = "/home/carlos/PycharmProjects/GenreRecognition/tmp/Slices/unknown/"
+
+tmp_dir = "./tmp/"
+shutil.rmtree(tmp_dir)
+os.makedirs(tmp_dir)
+raw_dir = os.makedirs(tmp_dir+'Raw/')
+spec_dir = os.makedirs(tmp_dir+'Spectrograms/')
+img_dir = os.makedirs(tmp_dir+'Slices/')
+
+# Convert audio to spectrogram
+audio2Image.convert_to_spectrogram(raw_dir, spec_dir)
+audio2Image.convert_to_slices(spec_dir, img_dir)
+
+
 
 predictions = []
 
 '''ciclo for para todos los archivos en tmp_dir
 
-        predict_image(model, image, image_size)
+        predict_image(model, img_dir, image_size)
 '''
 
 '''
     Luego clacula promedio
 '''
+
+shutil.rmtree(tmp_dir)
